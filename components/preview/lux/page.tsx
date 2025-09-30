12c2c1a17a72ed700ae47008b6be584b91203be3
nick maxwell <drnickmaxwell@gmail.com>
2025-09-29 22:30:46 -0700

    fix: add 'use client', proper imports, and alias corrections to preview/lux page

--- components/preview/lux/page.tsx
+++ components/preview/lux/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+import HeroLuxe from '@/components/preview/lux/HeroLuxe';
+import StatsLuxe from '@/components/preview/lux/StatsLuxe';
 import FeaturesLuxe from '@/components/preview/lux/FeaturesLuxe';
 import JourneyLuxe from '@/components/preview/lux/JourneyLuxe';
 import TestimonialsLuxe from '@/components/preview/lux/TestimonialsLuxe';
